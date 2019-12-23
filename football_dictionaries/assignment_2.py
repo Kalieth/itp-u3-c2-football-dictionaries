6d5a25354a1e13155f9cf9d12c118e11c5e4d8fa
Nakahi <15509939+Kalieth@users.noreply.github.com>
2019-12-23 02:44:05 +0000

    Add files via upload

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,18 @@
 def players_by_position(squads_list):
-    pass
+    players = {}
+    for player in squads_list:
+        position = player[1]
+        players.setdefault(position,[])
+        players[position].append(
+            {
+            'number': player[0],
+            'position': position,
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8]
+        })
+    return players
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,20 @@
 def players_by_country_and_position(squads_list):
-    pass
+    players = {}
+    for player in squads_list:
+        country = player[6]
+        position = player[1]
+        players.setdefault(country,{})
+        players[country].setdefault(position, [])
+        players[country][position].append(
+            {
+            'number': player[0],
+            'position': position,
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': country,
+            'club_country': player[7],
+            'year': player[8]
+        })
+    return players
